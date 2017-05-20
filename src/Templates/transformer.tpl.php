9bc515032be9d8de044c282dcf44daefd5ae586a
Trịnh Quốc Hoàng <quochoangvp@gmail.com>
2017-05-20 10:57:49 +0700

    Change transformer template

--- src/Templates/transformer.tpl.php
+++ src/Templates/transformer.tpl.php
@@ -10,8 +10,9 @@ class [[controller_name]]Transformer extends TransformerAbstract
 {
     public function transform([[model_uc]] $[[model_singular]]) {
         return [
-            'id' => (int) $[[model_singular]]->id,
-            'title' => (string) $[[model_singular]]->title,
+[[foreach:columns]]
+            '[[i.name]]' => $[[model_singular]]->[[i.name]],
+[[endforeach]]
         ];
     }
 }
